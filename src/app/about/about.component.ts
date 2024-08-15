6ec646c3e10687aca82bfb899bf38949d4ce8800
Pai <pai.nuneza@gmail.com>
2024-08-16 03:35:43 +0800

    Updates

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,62 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
 })
 export class AboutComponent {
-
+  teamMembers = [
+    {
+      name: 'Holden Caulfield',
+      role: 'UI Developer',
+      description:
+        'DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.',
+      imageUrl: 'https://showcase.empower.net/wp-content/uploads/2019/08/team-member-02.jpg',
+      socialLinks: {
+        facebook: '#',
+        twitter: '#',
+        linkedin: '#',
+      },
+    },
+    {
+      name: 'Alper Kamu',
+      role: 'Designer',
+      description:
+        'DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.',
+      imageUrl: 'https://showcase.empower.net/wp-content/uploads/2019/08/team-member-03.jpg',
+      socialLinks: {
+        facebook: '#',
+        twitter: '#',
+        linkedin: '#',
+      },
+    },
+    {
+      name: 'Atticus Finch',
+      role: 'UI Developer',
+      description:
+        'DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.',
+      imageUrl: 'https://showcase.empower.net/wp-content/uploads/2019/08/team-member-04.jpg',
+      socialLinks: {
+        facebook: '#',
+        twitter: '#',
+        linkedin: '#',
+      },
+    },
+    {
+      name: 'Heny Letham',
+      role: 'Designer',
+      description:
+        'DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.',
+      imageUrl: 'https://showcase.empower.net/wp-content/uploads/2019/08/team-member-01.jpg',
+      socialLinks: {
+        facebook: '#',
+        twitter: '#',
+        linkedin: '#',
+      },
+    },
+  ];
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -38,7 +38,5 @@ export class HomeComponent {
       title: 'Christmas Day',
     },
   ];
-
-
   
 }
