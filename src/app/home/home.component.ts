5846ab7fe60649198174d7f4481560c09dc18ce0
Pai <pai.nuneza@gmail.com>
2024-09-04 14:52:16 +0800

    updates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import holidaysData from '../../assets/data/holidays.json';
 import eventsData from '../../assets/data/events.json';
 import { RouterModule } from '@angular/router';
+import { SwiperOptions } from 'swiper/types';
 
 @Component({
   selector: 'app-home',
@@ -16,6 +17,7 @@ export class HomeComponent {
   events = eventsData;
   holidays = holidaysData;
   showMore = false;
+  backgroundImageUrl = 'https://media.nomadicmatt.com/2024/sydneythings1.jpeg';
 
   toggleReadMore() {
     this.showMore = !this.showMore;
